797568056e22339aa6aedabcc44a9f5ef6082699
Kevin Herron <kevinherron@gmail.com>
2025-01-08 13:34:06 -0800

    ~ config validation

--- msd-gateway/src/main/java/com/kevinherron/ignition/modbus/ModbusServerDeviceExtensionPoint.java
+++ msd-gateway/src/main/java/com/kevinherron/ignition/modbus/ModbusServerDeviceExtensionPoint.java
@@ -1,5 +1,6 @@
 package com.kevinherron.ignition.modbus;
 
+import com.inductiveautomation.ignition.gateway.config.ValidationErrors;
 import com.inductiveautomation.ignition.gateway.opcua.server.api.Device;
 import com.inductiveautomation.ignition.gateway.opcua.server.api.DeviceContext;
 import com.inductiveautomation.ignition.gateway.opcua.server.api.DeviceExtensionPoint;
@@ -13,17 +14,48 @@ public class ModbusServerDeviceExtensionPoint
         "com.kevinherron.modbus-server-driver",
         "ModbusServer.ModbusServerDeviceType.Name",
         "ModbusServer.ModbusServerDeviceType.Desc",
-        ModbusServerDeviceConfig.class
-    );
+        ModbusServerDeviceConfig.class);
   }
 
   @Override
   protected Device createDevice(
       DeviceContext deviceContext,
       DeviceProfileConfig profileConfig,
-      ModbusServerDeviceConfig deviceConfig
-  ) {
+      ModbusServerDeviceConfig deviceConfig) {
 
     return new ModbusServerDevice(deviceContext, deviceConfig);
+  }
+
+  @Override
+  protected void validate(ModbusServerDeviceConfig settings, ValidationErrors.Builder errors) {
+    errors.checkField(
+        settings.connectivity().port() > 0 && settings.connectivity().port() < 65536,
+        "connectivity.port",
+        "port must be between 1 and 65535");
+
+    try {
+      BrowsableAddressSpace.parseRanges(settings.browsing().coilBrowseRanges());
+    } catch (Exception e) {
+      errors.addFieldMessage("browsing.coilBrowseRanges", "invalid coil ranges");
+    }
+
+    try {
+      BrowsableAddressSpace.parseRanges(settings.browsing().discreteInputBrowseRanges());
+    } catch (Exception e) {
+      errors.addFieldMessage("browsing.discreteInputBrowseRanges", "invalid discrete input ranges");
+    }
+
+    try {
+      BrowsableAddressSpace.parseRanges(settings.browsing().holdingRegisterBrowseRanges());
+    } catch (Exception e) {
+      errors.addFieldMessage(
+          "browsing.holdingRegisterBrowseRanges", "invalid holding register ranges");
+    }
+
+    try {
+      BrowsableAddressSpace.parseRanges(settings.browsing().inputRegisterBrowseRanges());
+    } catch (Exception e) {
+      errors.addFieldMessage("browsing.inputRegisterBrowseRanges", "invalid input register ranges");
+    }
   }
 }
